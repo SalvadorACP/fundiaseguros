014137b6cbdfdb6541ab70c636b89183168eec8a
SalvadorCA <cruz.pizano.sa@gmail.com>
2026-08-27 01:43:56 -0600

    iniciamos fundiaseguros

---
+++ src/config/site.ts
@@ -0,0 +1,23 @@
+export const siteConfig = {
+  name: "Fundiaseguros",
+
+  description:
+    "Estamos trabajando en una nueva experiencia digital.",
+
+  contact: {
+    phone: "6693252858",
+    email: "gilbertoayala@fundiaseguros.com",
+    whatsapp: "https://wa.me/526693252858",
+  },
+
+  location: {
+    address: "Laguna Plaza & Business Center, Av. Reforma 2007-B, Flamingos, 82149 Mazatlán, Sin.",
+    mapsUrl: "https://www.google.com/maps/place/Fundia+Seguros/@23.2374442,-106.4386472,17z/data=!3m1!4b1!4m6!3m5!1s0x869f539fbd70b1ff:0xc1cee1d8208151ac!8m2!3d23.2374442!4d-106.4386472!16s%2Fg%2F11nvdfmmvw!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDgyNC4wIKXMDSoASAFQAw%3D%3D",
+  },
+
+  social: {
+    facebook: "https://www.facebook.com/ayalaasesores.agente",
+    instagram: "https://instagram.com/aseguro_tus_finanzas",
+    linkedin: "https://www.linkedin.com/in/josegilbertoayalarosales/",
+  },
+} as const;
